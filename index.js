d45c0af2a747af55a52e62114a2603d11b1e0093
Juan Ferreira <juanribeiro30@gmail.com>
2022-06-11 12:03:05 -0300

    Code Refactor

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function btnClicked() {
-    const todoInput = document.getElementById('todoInput');
+const todoInput = document.getElementById('todoInput');
+
+todoInput.addEventListener('keyup', onTextEnter)
+
+function onTextEnter(event) {
+    if (event.key != 'Enter' || event.keyCode !=13 || todoInput.value.trim() === ""){
+        return; 
+    }
+
+
     const list = document.getElementById('list');
     const inputValue = todoInput.value;
 
     const newTodo = document.createElement('li');
+    newTodo.classList.add('row')
     const newContent = document.createTextNode(inputValue);
 
     newTodo.appendChild(newContent);
